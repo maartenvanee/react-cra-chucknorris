23a83ab975da846359db29d312bb77ea8b3ee510
Maarten van Ee <maartenvanee@gmail.com>
2018-10-05 15:01:56 +0200

    basis

---
+++ build/precache-manifest.e70d476b1d9f58b30277127aa7b738d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "958ddde748345c1d03b8",
+    "url": "/static/js/main.958ddde7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "958ddde748345c1d03b8",
+    "url": "/static/css/main.34216503.chunk.css"
+  },
+  {
+    "revision": "f9aed4c238e9e83357c518e6ce15130b",
+    "url": "/index.html"
+  }
+];
